aadf54c1cc089ff7ab4791397f9575e4fa86dd9f
Kolarek Tony <tony.kolarek@student.supsi.ch>
2023-10-02 16:50:29 +0200

    test: added tests for CommandLineModel

--- client/src/main/java/ch/supsi/fsci/client/view/CommandLineView.java
+++ client/src/main/java/ch/supsi/fsci/client/view/CommandLineView.java
@@ -3,7 +3,6 @@ package ch.supsi.fsci.client.view;
 import javafx.scene.control.TextField;
 
 public class CommandLineView implements CommandLineViewInterface {
-
     @Override
     public void clearText(TextField textField) {
         textField.clear();
--- client/src/test/java/ch/supsi/fsci/client/model/CommandLineModelTest.java
+++ client/src/test/java/ch/supsi/fsci/client/model/CommandLineModelTest.java
@@ -1,7 +1,24 @@
 package ch.supsi.fsci.client.model;
-
+import ch.supsi.fsci.engine.Model.FileSystemModel;
 import org.junit.jupiter.api.Test;
 
-public class CommandLineModelTest {
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommandLineModelTest {
+    @Test
+    public void testSetText() {
+        final CommandLineModel commandLineModel = new CommandLineModel(new FileSystemModel());
+        assertEquals(commandLineModel.setText("cdd should be an error"), "error");
+        assertEquals(commandLineModel.setText("pwd"), "");
+        assertEquals(commandLineModel.setText("mkdir test test2"), "");
+        assertEquals(commandLineModel.setText("ls"), "");
+        assertEquals(commandLineModel.setText("cd \\A"), "changed directory to: A");
+        assertEquals(commandLineModel.setText("mv test test1"), "");
+        assertEquals(commandLineModel.setText("rm test"), "");
+        assertEquals(commandLineModel.setText("help"), "");
+        assertEquals(commandLineModel.setText("clear"), "");
+    }
 }
