ace81f59e4892d7fe74f74ce901ff1aaddbed0f4
Samuele Saporito <samuele.saporito@student.supsi.ch>
2023-10-05 18:45:43 +0200

    Creazione della cartella GUI ed inserito dentro le preference

---
+++ engine/src/main/java/ch/supsi/fsci/engine/Model/Persistence.java
@@ -0,0 +1,82 @@
+package ch.supsi.fsci.engine.Model;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Persistence {
+    public static final String PREFERENCE_FILE_PATH = System.getProperty("user.home") + File.separator + "GUI" + File.separator + "preferences.txt";
+    private Preference preference;
+    private boolean initialized;
+
+    private synchronized static void createFolderGUI() {
+        try {
+            Path defaultPath = Paths.get(PREFERENCE_FILE_PATH).getParent();
+            Files.createDirectories(defaultPath);
+            Files.setAttribute(defaultPath, "dos:hidden", true);
+        } catch (IOException e) {
+            System.out.println("createFolderGUI: " + e.getMessage());
+        }
+    }
+
+    private void savePreference() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PREFERENCE_FILE_PATH))) {
+            writer.write("\"language\": \"" + preference.getLanguage() + "\"\n");
+            writer.write("\"prefCommandSpacerWidth\": " + preference.getPrefCommandSpacerWidth() + "\n");
+            writer.write("\"commandFieldPrefColumnCount\": " + preference.getCommandFieldPrefColumnCount() + "\n");
+            writer.write("\"prefOutputAreaRowCount\": " + preference.getPrefOutputAreaRowCount() + "\n");
+            writer.write("\"prefInsetsSize\": " + preference.getPrefInsetsSize() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //public
+    public void initializeExplicit(){
+        File preferenceFile = new File(PREFERENCE_FILE_PATH);
+        if (preferenceFile.exists()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(preferenceFile))) {
+                String line;
+                Preference preference = new Preference();
+                while ((line = br.readLine()) != null) {
+                    String[] parts = line.split(":");
+                    if (parts.length == 2) {
+                        String key = parts[0].trim();
+                        String value = parts[1].trim();
+                        preference.setPreference(key, value);
+                    }
+                }
+                this.preference = preference;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            this.preference = new Preference();
+            createFolderGUI();
+            savePreference();
+        }
+        this.initialized = true;
+    }
+
+    public HashMap<String, String> getPreference() {
+        HashMap<String, String> preferencesData = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(PREFERENCE_FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+                    preferencesData.put(key, value);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return preferencesData;
+    }
+
+
+}
---
+++ engine/src/main/java/ch/supsi/fsci/engine/Model/Preference.java
@@ -0,0 +1,60 @@
+package ch.supsi.fsci.engine.Model;
+
+public class Preference {
+    private String language;
+    private int prefCommandSpacerWidth;
+    private int commandFieldPrefColumnCount;
+    private int prefOutputAreaRowCount;
+    private int prefInsetsSize;
+
+    public Preference() {
+        this.language = "en";
+        this.prefCommandSpacerWidth = 11;
+        this.commandFieldPrefColumnCount = 80;
+        this.prefOutputAreaRowCount = 25;
+        this.prefInsetsSize = 7;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public int getPrefCommandSpacerWidth() {
+        return prefCommandSpacerWidth;
+    }
+
+    public int getCommandFieldPrefColumnCount() {
+        return commandFieldPrefColumnCount;
+    }
+
+    public int getPrefOutputAreaRowCount() {
+        return prefOutputAreaRowCount;
+    }
+
+    public int getPrefInsetsSize() {
+        return prefInsetsSize;
+    }
+
+    // Metodo per impostare i valori delle preferenze
+    public void setPreference(String key, String value) {
+        switch (key) {
+            case "language":
+                this.language = value;
+                break;
+            case "prefCommandSpacerWidth":
+                this.prefCommandSpacerWidth = Integer.parseInt(value);
+                break;
+            case "commandFieldPrefColumnCount":
+                this.commandFieldPrefColumnCount = Integer.parseInt(value);
+                break;
+            case "prefOutputAreaRowCount":
+                this.prefOutputAreaRowCount = Integer.parseInt(value);
+                break;
+            case "prefInsetsSize":
+                this.prefInsetsSize = Integer.parseInt(value);
+                break;
+        }
+
+
+    }
+}
