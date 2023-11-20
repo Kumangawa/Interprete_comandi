ecb8407e27a4d73ab965355a836a614c8933613d
Kolarek Tony <tony.kolarek@student.supsi.ch>
2023-11-20 15:44:20 +0100

    removed external (useless) pom.xml

--- client/src/main/java/ch/supsi/fsci/client/controller/CommandExecutionController.java
+++ client/src/main/java/ch/supsi/fsci/client/controller/CommandExecutionController.java
@@ -38,6 +38,7 @@ public class CommandExecutionController {
         try {
             final CommandInterface command = commandExecutionModel.getDispatchedCommand(input);
             return command.execute();
+            // Get response
         }
         catch (final Exception e) {
             e.printStackTrace();
